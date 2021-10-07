4c1b5465ed0076eea3ccc1fe6771827a231f2843
merttuncel <82678965+merttuncel3@users.noreply.github.com>
2021-10-07 16:18:14 +0300

    A.03.1- First 5 tasks has been done.
    I forgot to make small commits, I will later on.

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -2,12 +2,75 @@ using UnityEngine;
 
 public class SpeedRacer : MonoBehaviour
 {
+    string carModel = "GTR R35";
+    string engineType = "V6, Twin Turbo";
+
+    int carWeight = 1609;
+    int yearMade = 2009;
+
+    float maxAcceleration = 0.98f;
+
+    bool isCarTypeSedan = false;
+    bool hasFrontEngine = true;
+
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        print("The vehicle is " + carModel + ". It has " + engineType);
+
+        CheckWeight();
+
+
+        if (yearMade < = 2009)
+        {
+            print("The vehicle was introduced in " + yearMade)
+
+            int carAge = CalculateAge(yearMade);
+            print("The vehicle is " + carAge + " years old.");
+
+        }
+
+        else
+        {
+            print("The vehicle was introduced in 2010.")
+            print("The vehicle's maximum acceleration is " + maxAcceleration)
+        }
+
+
+
     }
+
+
+    void CheckWeight()
+    {
+      if(carWeight < 1500)
+
+        {
+            print("The " + carModel + " weights less than 1500kg");
+        }
+       
+      else
+
+        {
+            print("The " + carModel + " weights more than 1500kg");
+        }
+    }
+
+
+
+    int CalculateAge(int yearMade)
+
+    {
+        return 2021 - yearMade;
+    }
+
+
+
 
     // Update is called once per frame
     void Update()
