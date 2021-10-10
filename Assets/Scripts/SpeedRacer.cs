2a26a726d7e43261322215f6b0ad8bad653939e8
Fifi Arisandi <fifi_arisandi@yahoo.com>
2021-10-10 23:41:42 +0300

    print carmodel, enginetype

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -9,11 +9,13 @@ public class SpeedRacer : MonoBehaviour
     float maxAcceleration = 0.98f;
     bool isCarTypeSedan = false;
     bool hasFrontEngine = true;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        print(carModel);
+        print(engineType);
+ 
     }
 
     // Update is called once per frame
