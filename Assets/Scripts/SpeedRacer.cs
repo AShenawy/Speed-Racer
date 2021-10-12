a4c331688041a83acf182e741bbfe13e4f425e29
alisafek <alisa.feklicheva@gmail.com>
2021-10-12 11:08:15 +0300

    Made public variables
    1. Took into account comments to the previous work.
    2. New variable carMaker appeared.
    3. The variables became public and were changed in the Unity inspector.
    4. Saved Unity file to stop creating a game object every time.

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -2,35 +2,22 @@ using UnityEngine;
 
 public class SpeedRacer : MonoBehaviour
 {
-    string carModel = "GTR R35";
-    string engineType = "V6, Twin Turbo";
-    /*
-    int carWeight = 1609;
-    int yearMade = 2009;
-    */
-    int carWeight = 1609;
-    int yearMade = 2009;
+    public string carModel = "GTR R35";
+    public string engineType = "V6, Twin Turbo";
+    public int carWeight = 1609;
+    public int yearMade = 2009;
+    public float maxAcceleration = 0.98f;
+    public bool isCarTypeSedan = false;
+    public bool hasFrontEngine = true;
 
-    float maxAcceleration = 0.98f;
-    bool isCarTypeSedan = false;
-    bool hasFrontEngine = true;
+    public string carMaker;
 
-    /*
-    float maxAcceleration = 0.98f;
-    bool isCarTypeSedan = false;
-    bool hasFrontEngine = true;
-    */
+ 
 
     // Start is called before the first frame update
     void Start()
     {
-        /*yearMade = 2009;
-        maxAcceleration = 0.98f;
-        isCarTypeSedan = false;
-        hasFrontEngine = true;*/
-
-        print(carModel);
-        print(engineType);
+        print(carModel+carMaker+engineType);
 
         //Debug.Log(carModel);
         //Debug.Log(engineType);
@@ -61,38 +48,44 @@ public class SpeedRacer : MonoBehaviour
     }
 
     void CheckWeight()
+    {
+        if (carWeight < 1500)
         {
-            if (carWeight < 1500)
+            print(carModel+"weighs less than 1500 kg.");
+        }
+        else
         {
-            print("The carModel weighs less than 1500 kg.");
+            print(carModel+"weighs over than 1500 kg.");
         }
-            else
-        {
-            print("The carModel weighs over than 1500 kg.");
-        }
-        }
+    }
 
     int CalculateAge(int yearMade)
-        {
-            int result;
-            result = 2021 - yearMade;
-            return result;
-        }
+    {
+        int result;
+        result = 2021 - yearMade;
+        return result;
+    }
 
     string CheckCharacteristics()
-        {
+    {
         if (isCarTypeSedan)
+        {
             return "Car Type i Sedan";
+        }
         else if (hasFrontEngine)
+        {
             return "the car isn’t a sedan, but has a front engine";
+        }
         else
+        {
             return "that the car is neither a sedan nor does it have a front engine";
         }
+    }
 
 
         // Update is called once per frame
     void Update()
-        {
+    {
 
-        }
     }
+}
