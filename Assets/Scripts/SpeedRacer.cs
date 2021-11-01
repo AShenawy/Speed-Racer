38a155514f3032000708d4a318f6f5eb7e803ee3
mannekeen <83460982+mannekeen@users.noreply.github.com>
2021-11-01 16:57:41 +0200

    Update SpeedRacer.cs

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -2,7 +2,18 @@ using UnityEngine;
 
 public class SpeedRacer : MonoBehaviour
 {
+    // Declare and initialise the car's information
 
+    string carModel = "GTR R35";
+    string engineType = "V6, Twin Turbo";
+
+    int carWeight = 1609;
+    int yearMade = 2009;
+
+    float maxAcceleration = 0.98f;
+
+    bool isCarTypeSedan = false;
+    bool hasFrontEngine = true;
     // Start is called before the first frame update
     void Start()
     {
