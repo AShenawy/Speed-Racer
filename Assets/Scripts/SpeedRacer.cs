d96bf4671a4b98903a2471c26259edd7133ef809
jsapoval <90786236+jsapoval@users.noreply.github.com>
2021-11-06 17:07:24 +0200

    i have error in script
    tried twice, even by copy/paste Answer script, same error

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -2,16 +2,68 @@ using UnityEngine;
 
 public class SpeedRacer : MonoBehaviour
 {
+    string carModel = "GTR R35";
+    string engineType = "V6, Twin Turbo";
+
+    int carWeight = 1609;
+    int yearMade = 2006;
+
+    float maxAcceleration = 0.98f;
+    bool isCarTypeSedan = false;
+    bool hasFrontEngine = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        print("The racer moldes is " + carModel + ". It has a " + engineType + "engine.");
         
+        CheckWeight();
+
+        if (yearMade >= 2009)
+        {
+            print(carModel + "was first introduced in " + yearMade);
+            int carAge = CalculateAge(yearMade);
+            print("This car is " + carAge + " years old.");
+
+        } else
+        {
+            print(carModel + "was introduced in the 2010's".);
+            print("Its maximum acceleration is " + maxAcceleration + "m/s2");
+        }
+
+        print(CheckCharacteristics());
+    }
+
+    void CheckWeight()
+    {
+        if (carWeight < 1500)
+        {
+            print("The " + carModel + " weight less than 1500 kg.");
+        }     
+        else 
+        {
+            print("The " + carWeight + " weights over 1500 kg.");
+        }
+    }
+
+    int CalculateAge(int yearMade)
+    {
+        return 2021 - yearMade;
     }
 
     // Update is called once per frame
-    void Update()
+    string CheckCharacteristics()
     {
-       
+       if (isCarTypeSedan)
+       {
+            return "Car type is sedan.";
+
+       } else if (hasFrontEngine)
+       {
+              return "The car is not a sedan, but has a front engine.";
+       } else
+       {
+              return "The car is neither a sedan nor its engine is a front.";
+       }
     }
 }
